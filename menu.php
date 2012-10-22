2fcd6e8b0f934e0e16ddcd37f9979f46c028e79c
Asmah-Jalali <Jalali@VAIO.(none)>
2012-10-22 08:56:48 +0330

    add menu

---
+++ menu.php
@@ -0,0 +1,36 @@
+<div class="menu">
+                <ul>
+                   <?php 
+					   wp_list_pages(array(
+							'title_li' => ''
+						))
+	//					wp_nav_menu(array(
+	//						'theme_location'  => 'mainmenu',
+	//						'menu'            => '', 
+	//						'container'       => 'div', 
+	//						'container_class' => 'menu-{menu slug}-container', 
+	//						'container_id'    => '',
+	//						'menu_class'      => 'nav', 
+	//						'menu_id'         => '',
+	//						'echo'            => true,
+	//						'fallback_cb'     => 'wp_page_menu',
+	//						'before'          => '',
+	//						'after'           => '',
+	//						'link_before'     => '',
+	//						'link_after'      => '',
+	//						'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+	//						'depth'           => 2,
+	//						'walker'          => ''
+	//					));
+
+				  ?>
+				</ul>
+                <?php
+    				dynamic_sidebar("Sidebar");
+    			?>
+
+                </ul>
+            </div>
+        </div>
+        <div class="divider">
+        </div>
