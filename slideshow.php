1cd584747ef35267f5443f5ebfe451711c3cf6ac
Asmah-Jalali <Jalali@VAIO.(none)>
2012-10-22 08:57:51 +0330

    add slideshow

---
+++ slideshow.php
@@ -0,0 +1,49 @@
+ <div class="slideshow-bg">
+        	 <div class="slideshow">
+                <div class="train">
+                    <div class="n" style="background-color:rgb(84,137,174);">
+                    	<div class="pic1"></div>
+                        <div class="txt">
+                        	<div class="txt1"></div>
+                            <div class="txt2"></div>
+                            <div class="txt3"></div>
+                            <div class="txt4"></div>
+                        </div>
+                    </div>
+                    <div style="background-color:rgb(84,137,174);">
+                    	<div class="pic2">
+                        	<div class="pic2-1"></div>
+	                       	<div class="pic2-2"></div>
+                           	<div class="pic2-3"></div>
+                        </div>
+                        <div class="txt">
+                        	<div class="txt5"></div>
+                            <div class="txt6"></div>
+                            <div class="txt7"></div>
+                        </div>
+                    </div>
+                    <div style="background-color:rgb(84,137,174);">
+                    	<div class="pic3">
+                        	<div class="pic3-1"></div>
+	                       	<div class="pic3-2"></div>
+                           	<div class="pic3-3"></div>
+                         	<div class="pic3-4"></div>
+                        </div>
+                        <div class="txt">
+                        	<div class="txt8"></div>
+                            <div class="txt9"></div>
+                            <div class="txt10"></div>
+                            <div class="txt11"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="next"></div>
+                <div class="previous"></div>
+                <ul class="btn">
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                </ul>
+             </div>
+                    
+        </div>
