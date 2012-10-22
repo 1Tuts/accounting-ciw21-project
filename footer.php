31be658c55b17480bae8e90fb9975b3b6956106c
Asmah-Jalali <Jalali@VAIO.(none)>
2012-10-22 08:53:51 +0330

    add footer

---
+++ footer.php
@@ -0,0 +1,10 @@
+   <div class="top-scroll">
+            	top scroll
+            </div>      	
+        </div>
+        <div class="footer">
+       			footer
+        </div>
+    </div>
+</body>
+</html>
