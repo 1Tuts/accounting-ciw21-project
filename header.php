2ab4816225adc303cc2346f0c9cf11ae6132f9e2
Asmah-Jalali <Jalali@VAIO.(none)>
2012-10-22 08:54:13 +0330

    add header

---
+++ header.php
@@ -0,0 +1,28 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>
+	<?php bloginfo('name'); wp_title();?>
+</title>
+<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url'); ?>/reset.css" />
+<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url'); ?>/style.css" />
+<script type="text/javascript" src="<?php bloginfo('template_url');?>/script.js"></script>
+<script type="text/javascript" src="<?php bloginfo('template_url');?>/zepto.js"></script>
+
+
+</head>
+
+<body>
+	<div class="body">
+    	<div class="header">
+            <div class="logo">
+            </div>
+            <div class="social-icon">
+                	<a href="#" class="social-icon1"></a>
+                    <a href="#" class="social-icon2"></a>
+                    <a href="#" class="social-icon3"></a>
+                    <a href="#" class="social-icon5"></a>
+                    <a href="#" class="social-icon6"></a>
+                    <a href="#" class="social-icon7"></a>
+            </div>
+            
