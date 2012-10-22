cf42404c68e20f1c5ccd2d30f8df0e1e3525e2ce
Asmah-Jalali <Jalali@VAIO.(none)>
2012-10-22 08:55:06 +0330

    add a specail page for a page

---
+++ index-product.php
@@ -0,0 +1,15 @@
+<?php
+	/*
+		Template Name:product
+	*/
+	get_template_part('header');
+
+	get_template_part('menu');
+
+	get_template_part('container');
+
+   	get_template_part('animate-box');
+	
+   	get_template_part('footer');
+
+   
