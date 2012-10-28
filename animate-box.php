9cd707f0bccc5b4b04fc4674bf9cabd87472f624
mahnaz.najafiyan <mahnaz.najafiyan@gmail.com>
2012-10-28 04:28:23 +0330

    corect the php code

--- animate-box.php
+++ animate-box.php
@@ -7,19 +7,22 @@
                 
              <div class="slideshowbtm">
                  <div class="trainbtm">
-                     <?php
-                         $images_path ='./images2';
-                         $images =scandir($images_path);
-                         $slidesLen = 0;
-                         foreach($images as $img){
-                            $img_arr = explode('.', $img);
-                             $img_type = strtolower( end($img_arr) );
-                            if ($img_type=='jpg') {
-                            echo "<div style=\"background-image:url('$images_path/$img');\"></div>";
-                            $slidesLen++;
-                             }
-                         }
-                    ?>
+                    <?php
+                        $images_path ='./images2';
+                        $template_dir_on_disk = get_template_directory();
+                        $template_web_url = get_bloginfo('template_url');
+
+                        $images = scandir( $template_dir_on_disk . $images_path);
+                        $slidesLen = 0;
+                        foreach ($images as $img) {
+                            $img_type = strtolower( end( explode( '.' , $img ) ) );
+                            if($img_type == 'jpg' || $img_type == 'png'){
+                                echo "<div style=\"background-image : url('$template_web_url/$images_path/$img');\"></div>";
+                                $slidesLen++;
+                            }
+                        }
+                    ?>    
+                     
                  </div>
              </div>
 </div>
