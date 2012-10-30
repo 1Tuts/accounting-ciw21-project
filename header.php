f944fa237f488c7259c6e78d52ed5de1f927fd2b
mahnaz.najafiyan <mahnaz.najafiyan@gmail.com>
2012-10-30 16:44:36 +0330

    add script of slide show btm

--- header.php
+++ header.php
@@ -7,6 +7,7 @@
 <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url'); ?>/1styles.css" />
 <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url'); ?>/style.css" />
 <script type="text/javascript" src="<?php bloginfo('template_url');?>/script.js"></script>
+<script type="text/javascript" src="<?php bloginfo('template_url');?>/slideshowbtm.js"></script>
 <script type="text/javascript" src="<?php bloginfo('template_url');?>/zepto.js"></script>
 
 
