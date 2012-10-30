43b7882fba7a191bd595b730829bf985c7afba34
mahnaz.najafiyan <mahnaz.najafiyan@gmail.com>
2012-10-30 17:47:13 +0330

    opacity of animate-box

--- slideshowbtm.js
+++ slideshowbtm.js
@@ -5,10 +5,12 @@ $(document).ready(function(){
 	slides.css({width:slidesWidth+'px'});
 	slides.last().css({'padding-right':'200px'});
 	slides.mouseover(function(){
+		slides.css({opacity:1});
 		slides.animate({width:'60px'},500);
 		$(this).animate({width:'600px'},500);
 	});
 	$('div.slideshowbtm').mouseout(function(){
+		slides.css({opacity:0.65});
 		slides.animate({width:slidesWidth+'px'},500);
 	});
 })
