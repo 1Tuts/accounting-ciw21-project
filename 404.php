3bce0fd13203d163f2f8e2ecd27994be7b4051e9
Asmah-Jalali <Jalali@VAIO.(none)>
2012-11-01 03:26:18 +0330

    404.php

---
+++ 404.php
@@ -0,0 +1,14 @@
+<?php
+	get_template_part('header');
+
+	get_template_part('menu');
+?>
+<article>
+	<h1>Page not Found</h1>
+	<p>The requested URL was not found on this server. If you entered the URL manually please check your spelling and try again.</p>
+<article>
+<?php
+	get_template_part('follow-us');
+	
+   	get_template_part('footer');
+?>
