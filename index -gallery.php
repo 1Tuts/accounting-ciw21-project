262d4329a73996b574643ad95668af8f0890b438
Asmah-Jalali <Jalali@VAIO.(none)>
2012-11-01 03:27:22 +0330

    gallery page

---
+++ index -gallery.php
@@ -0,0 +1,18 @@
+<?php
+	get_template_part('header');
+
+	get_template_part('menu');
+
+	get_template_part('slideshow');
+
+	get_template_part('container');
+
+   	get_template_part('animate-box');
+
+   	get_template_part('footer');
+
+   
+        
+    
+          
+         
