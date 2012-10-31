558331ca848b13009ef206326e052800fcb293f0
Asmah-Jalali <Jalali@VAIO.(none)>
2012-11-01 03:27:36 +0330

    About Me page

---
+++ index-aboutme.php
@@ -0,0 +1,23 @@
+<?php
+	/*
+		Template Name:About-Me
+	*/
+	get_template_part('header');
+
+	get_template_part('menu');
+	
+	get_template_part('slideshow');
+
+	get_template_part('container');
+
+   	get_template_part('animate-box');
+
+   	get_template_part('follow-us');
+	
+   	get_template_part('footer');
+
+   
+        
+    
+          
+         
