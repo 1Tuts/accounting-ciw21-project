da56c441be16c03ab3da44dbf486c3ac42813b51
Asmah-Jalali <Jalali@VAIO.(none)>
2012-11-01 03:29:26 +0330

    abit change

--- index-product.php
+++ index-product.php
@@ -5,10 +5,10 @@
 	get_template_part('header');
 
 	get_template_part('menu');
+	
+	get_template_part('slideshow');
 
 	get_template_part('container');
-
-   	get_template_part('animate-box');
 	
    	get_template_part('footer');
 
