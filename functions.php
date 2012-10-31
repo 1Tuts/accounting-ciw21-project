141b5d2e201b3ce509eb1f6c905faed9bfbcfc1a
Asmah-Jalali <Jalali@VAIO.(none)>
2012-11-01 03:27:04 +0330

    add register nav menu

--- functions.php
+++ functions.php
@@ -1,14 +1,16 @@
 <?php
 	add_theme_support('menus');
 
+	register_nav_menu('mainmenu', "Main Manu");
+
 	register_sidebar(array(
-		'name' => 'Sidebar',
+		'name' => 'Sidebar Left',
 		'id' => 'sidebar-l',
-		'description' => 'Top Menus',
+		'description' => 'Left panel after menu',
 		'before_widget' => '<article class="widget %2$s">',
 		'after_widget' => "</article>\n",
 		'before_title' => '<h2 class="widgettitle">',
 		'after_title' => "</h2>\n"
 	));
-	
-	
+
+add_theme_support('post-thumbnails');
