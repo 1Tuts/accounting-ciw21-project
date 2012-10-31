f75c8121a8ecadf426bd5c6633ff2d4eeaac0599
Asmah-Jalali <Jalali@VAIO.(none)>
2012-11-01 03:27:49 +0330

    Contact Me page

---
+++ index-contactme.php
@@ -0,0 +1,21 @@
+<?php
+	/*
+		Template Name:Contact-Me
+	*/
+	get_template_part('header');
+
+	get_template_part('menu');
+	
+	get_template_part('slideshow');
+
+	get_template_part('container');
+
+   	get_template_part('follow-us');
+	
+   	get_template_part('footer');
+
+   
+        
+    
+          
+         
