85c28cca7e8d7791187f6db0d1ec52e1e8ba87a6
Asmah-Jalali <Jalali@VAIO.(none)>
2012-11-01 03:29:55 +0330

    no thing

--- menu.php
+++ menu.php
@@ -1,37 +1,23 @@
-<div class="icon-menu">
 	<div class="menu">
-                <ul>
-                   <?php 
-					   wp_list_pages(array(
-							'title_li' => ''
-						))
-	//					wp_nav_menu(array(
-	//						'theme_location'  => 'mainmenu',
-	//						'menu'            => '', 
-	//						'container'       => 'div', 
-	//						'container_class' => 'menu-{menu slug}-container', 
-	//						'container_id'    => '',
-	//						'menu_class'      => 'nav', 
-	//						'menu_id'         => '',
-	//						'echo'            => true,
-	//						'fallback_cb'     => 'wp_page_menu',
-	//						'before'          => '',
-	//						'after'           => '',
-	//						'link_before'     => '',
-	//						'link_after'      => '',
-	//						'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
-	//						'depth'           => 2,
-	//						'walker'          => ''
-	//					));
-
-				  ?>
-				</ul>
-                <?php
-    				dynamic_sidebar("Sidebar");
-    			?>
-
-                </ul>
-            </div>
-        </div>
+        <nav>
+            <?php
+                wp_nav_menu(array(
+                    'theme_location'  => 'mainmenu',
+                    'container'       => false, 
+                    'container_class' => '', 
+                    'menu_class'      => '', 
+                    'before'          => '',
+                    'after'           => '',
+                    'link_before'     => '',
+                    'link_after'      => '',
+                    'items_wrap'      => '<ul class="%2$s">%3$s</ul>'
+                ));
+				?>
+			</nav>
+			<?php
+				dynamic_sidebar("Sidebar Left");
+			?>
+    </div>
     <div class="divider">
     </div>
+   
